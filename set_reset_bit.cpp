a23c7044f9bc9ab5f6e27b4a49108f280ff75c93
shravanighule <shravanighule16@gmail.com>
2024-08-01 10:59:41 +0530

    Add files via upload

---
+++ set_reset_bit.cpp
@@ -0,0 +1,32 @@
+//shravanighule 23070123056
+// to set or reset a particular bit 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int bit_set,bit_reset;
+    int i;
+    int set, reset;
+
+    cout<<"Enter the number to be set or reset: ";
+    cin>>i;
+
+    cout<<"Enter the bit to be changed: ";
+    cin>>bit_set>>bit_reset;
+
+    set = i|(1<<bit_set);                         //using or operator to convert from 0 to 1
+    reset = i&(~(1<<bit_reset));                    //using and operator to convert from 1 to 0
+
+    cout<<"set: "<<set<<endl;
+    cout<<"Reset: "<<reset<<endl;
+}
+
+/*Enter the number to be set or reset: 5
+Enter the bit to be changed: 1 2
+set: 7
+Reset: 1
+
+Enter the number to be set or reset: 7
+Enter the bit to be changed: 3 2
+set: 15
+Reset: 3*/
